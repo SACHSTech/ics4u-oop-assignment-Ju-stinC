614240d6dc2185b45a0590d2f5b7c6201d4eb5d6
Justin Chan <justin.chan21@ycdsbk12.ca>
2021-01-07 04:46:18 +0000

    user javadoc comments

--- User.java
+++ User.java
@@ -11,6 +11,13 @@ public class User {
   SavedPodcast savedPod;
   SavedAlbum savedAlbum;
 
+  /**
+  * Constructor for giving the user the objects that they will need
+  * @param SavedSong - the user may need a savedsong
+  * @param SavedPodcast - the user may need a savedpodcast
+  * @param SavedAlbum - the user may need a savevdalbum
+  */
+
   public User(SavedSong savedSongg, SavedPodcast savedPodd, SavedAlbum savedAlbumm) {
 
     savedSong = savedSongg;
@@ -19,11 +26,19 @@ public class User {
 
   }
 
+  /**
+  * @param fName for the user account first name
+  */
+
   public void setFirstName(String fName) {
 
     firstName = fName;
 
   }
+
+  /**
+  * @return firstName for what they entered as their first name
+  */
 
   public String getFirstName() {
 
@@ -31,11 +46,19 @@ public class User {
 
   }
 
+  /**
+  * @param lName for the user account last name
+  */
+
   public void setLastName(String lName) {
 
     lastName = lName;
 
   }
+
+  /**
+  * @return lastName for what they entered as their last name
+  */
 
   public String getLastName() {
 
@@ -43,11 +66,19 @@ public class User {
 
   }
 
+  /**
+  * @param followingNumber which is the number of accounts they want to follow
+  */
+
   public void setFollowing(int followingNumber) {
 
     followingCount = followingNumber;
 
   }
+
+  /**
+  * @return followingCount which is the number of accounts they want to follow
+  */
 
   public int getFollowing() {
 
@@ -55,17 +86,29 @@ public class User {
 
   }
 
+  /**
+  * @param followerNumber which is the number of accounts they want to be followed by
+  */
+
   public void setFollowers(int followerNumber) {
 
     followerCount = followerNumber;
 
   }
 
+  /**
+  * @return followerCount which is the number of accounts they want to be followed by
+  */
+
   public int getFollowers() {
 
     return followerCount;
 
   }
+
+  /** 
+  * after they enter their details, this method will print
+  */
 
   public void getAccountDetails() {
 
